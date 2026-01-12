e2d6b7384813754356c617b4f12d21805048078c
Fabian Diener <f46200@gmail.com>
2026-01-12 22:01:52 +0100

    Fix linter issues

--- App.tsx
+++ App.tsx
@@ -5,41 +5,40 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
+	SafeAreaProvider,
+	useSafeAreaInsets,
 } from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
+import {StatusBar, StyleSheet, View, useColorScheme} from 'react-native';
+import {NewAppScreen} from '@react-native/new-app-screen';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+	container: {
+		flex: 1,
+	},
 });
+
+function AppContent() {
+	const safeAreaInsets = useSafeAreaInsets();
+
+	return (
+		<View style={styles.container}>
+			<NewAppScreen
+				templateFileName="App.tsx"
+				safeAreaInsets={safeAreaInsets}
+			/>
+		</View>
+	);
+}
+function App() {
+	const isDarkMode = useColorScheme() === 'dark';
+
+	return (
+		<SafeAreaProvider>
+			<StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+			<AppContent />
+		</SafeAreaProvider>
+	);
+}
 
 export default App;
