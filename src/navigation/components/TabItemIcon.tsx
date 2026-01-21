0eed46d8c4638062659dd49ad73b9069f07ebe8d
Fabian Diener <f46200@gmail.com>
2026-01-21 22:25:52 +0100

    Color focused icon in bottom bar

--- src/navigation/components/TabItemIcon.tsx
+++ src/navigation/components/TabItemIcon.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
-import {Icon} from 'react-native-paper';
+import {Icon, useTheme} from 'react-native-paper';
 
-const TabItemIcon = ({icon}: {icon: string}) => {
-	return <Icon source={icon} size={20} />;
+const TabItemIcon = ({icon, focused}: {icon: string; focused: boolean}) => {
+	const theme = useTheme();
+
+	return (
+		<Icon
+			source={icon}
+			size={20}
+			color={focused ? theme.colors.primary : undefined}
+		/>
+	);
 };
 
 export const createTabIconFunction = (icon: string) => {
-	return () => <TabItemIcon icon={icon} />;
+	return ({focused}: {focused: boolean}) => (
+		<TabItemIcon icon={icon} focused={focused} />
+	);
 };
