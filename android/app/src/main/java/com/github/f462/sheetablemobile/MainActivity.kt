b3072b64926eb4dfba19e5ee824ec34de6859874
Fabian Diener <f46200@gmail.com>
2026-01-23 20:02:04 +0100

    Well-define app ID

--- android/app/src/main/java/com/github/f462/sheetablemobile/MainActivity.kt
+++ android/app/src/main/java/com/github/f462/sheetablemobile/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.sheetablemobile
+package com.github.f462.sheetablemobile
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/github/f462/sheetablemobile/MainApplication.kt
+++ android/app/src/main/java/com/github/f462/sheetablemobile/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.sheetablemobile
+package com.github.f462.sheetablemobile
 
 import android.app.Application
 import com.facebook.react.PackageList
